e55c8e3a44d708ff380fb5ed129526929f054686
TonyReignsT <tonyreigns00@gmail.com>
2024-09-17 06:41:58 +0300

    online form

---
+++ form.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.urlencoded({ extended: true })); // Enable URL-encoded data parsing
+
+// Database Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'User_Form'
+});
+
+// Connect to the database
+db.connect(err => {
+    if (err) throw err;
+    console.log("Connected to the database...");
+});
+
+// Serve the HTML form
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'details.html')); // Serve the HTML file
+});
+
+// Handle form submission
+app.post("/", (req, res) => {
+    const { fname, lname, gender, age } = req.body;
+
+    // Inserting data into the database
+    const query = 'INSERT INTO users (first_name, last_name, gender, age) VALUES (?, ?, ?, ?)';
+    db.query(query, [fname, lname, gender, age], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Database insertion error!!");
+        }
+        // Responding with a success message
+        res.send(`<h1>Data Insertion Successful!</h1>
+            <p><strong>First Name:</strong> ${fname}</p>
+            <p><strong>Last Name:</strong> ${lname}</p>
+            <p><strong>Gender:</strong> ${gender}</p>
+            <p><strong>Age:</strong> ${age}</p>
+            <a href="/">Go Back</a>`);
+    });
+});
+
+app.post("/", (req, res)=> {
+    // console.log(req.body.fname);  //log posted data
+    res.send("Post received!");
+})
+
+app.listen(3000, (err)=> {
+    if (err) throw err;
+    console.log("Server running on port 3000...");
+});
